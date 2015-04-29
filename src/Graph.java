0cf295702553e50d0848353fdd4962fa36d5ba97
phuong <phuong1492.bka@gmail.com>
2015-04-29 15:45:01 +0700

    create matrix weight for backup

--- src/Graph.java
+++ src/Graph.java
@@ -7,6 +7,7 @@ public class Graph {
 	public List<Edge> edge;
 	private boolean connected[][];
 	private float distance[][];
+	public float weight[][];
 
 	// public void getData(){
 	// GetData a = new GetData();
@@ -25,6 +26,7 @@ public class Graph {
 			this.e = 0;
 			this.v = v;
 			this.distance = distance;
+			weight = new float[node.size()][node.size()];
 			// Connected array is automatically initialized with “false”
 			connected = new boolean[v][v];
 		}
@@ -83,29 +85,31 @@ public class Graph {
 		check[src.getId()] = true;
 		// khoi tao mang khoang cach
 		for (int i = 0; i < node.size(); i++) {
-			//workpath[i] = i;
+			// workpath[i] = i;
 			if (connected[src.getId()][node.get(i).getId()]) {
 				minDistances[i] = distance[src.getId()][node.get(i).getId()];
-//				System.out.println(i + " : " + minDistances[node.get(i).getId()]);
+				// System.out.println(i + " : " +
+				// minDistances[node.get(i).getId()]);
 			} else {
 				if (node.get(i) != src) {
 					minDistances[i] = Float.MAX_VALUE;
-//					System.out.println(i + " : MAX");
-				}else {
+					// System.out.println(i + " : MAX");
+				} else {
 					minDistances[node.get(i).getId()] = 0;
 				}
 			}
 			if (node.get(i) != src)
 				check[node.get(i).getId()] = false;
-//			System.out.println("Node"+node.get(i).getId() + " " +check[node.get(i).getId()]);
+			// System.out.println("Node"+node.get(i).getId() + " "
+			// +check[node.get(i).getId()]);
 		}
-		
-		
+
 		List<Node> adj1 = adj(src);
-//		System.out.println(adj1);
+		// System.out.println(adj1);
 		for (int i = 0; i < adj1.size(); i++) {
 			workpath[adj1.get(i).getId()] = src.getId();
-//			System.out.println( adj1.get(i).getId() +">>>" + workpath[adj1.get(i).getId()]);
+			// System.out.println( adj1.get(i).getId() +">>>" +
+			// workpath[adj1.get(i).getId()]);
 		}
 		minDistances[src.getId()] = 0;
 
@@ -113,7 +117,8 @@ public class Graph {
 			float min = Float.MAX_VALUE;
 			int minV = -1;
 			for (int w = 0; w < node.size(); w++) {
-				if (minDistances[node.get(w).getId()] < min && !check[node.get(w).getId()]) {
+				if (minDistances[node.get(w).getId()] < min
+						&& !check[node.get(w).getId()]) {
 					minV = node.get(w).getId();
 					min = minDistances[node.get(w).getId()];
 				}
@@ -124,39 +129,83 @@ public class Graph {
 			check[minV] = true;
 
 			// Them v vao trong workpath
-//			System.out.println("---------------------------------");
-//			System.out.println("minV: " + minV + "-" + minDistances[minV]);
-//			List<Node> listAdj = adj(node.get(minV));
-//			System.out.println(" ==>>>>>>>>>");
-//			System.out.println(listAdj);
+			// System.out.println("---------------------------------");
+			// System.out.println("minV: " + minV + "-" + minDistances[minV]);
+			// List<Node> listAdj = adj(node.get(minV));
+			// System.out.println(" ==>>>>>>>>>");
+			// System.out.println(listAdj);
 			for (int w = 0; w < node.size(); w++) {
 				if (connected[node.get(minV).getId()][node.get(w).getId()]) {
 					// System.out.println(w+ " " + listAdj.get(w) + " " +
 					// minDistances[listAdj.get(w)]);
 
-					if (minDistances[node.get(w).getId()] > minDistances[node.get(minV).getId()]
-							+ distance[node.get(minV).getId()][node.get(w).getId()]) {
-//						System.out.println(node.get(w).getId() + " - " + minDistances[node.get(w).getId()]);
-						minDistances[node.get(w).getId()] = minDistances[node.get(minV).getId()]
-								+ distance[node.get(minV).getId()][node.get(w).getId()];
+					if (minDistances[node.get(w).getId()] > minDistances[node
+							.get(minV).getId()]
+							+ distance[node.get(minV).getId()][node.get(w)
+									.getId()]) {
+						// System.out.println(node.get(w).getId() + " - " +
+						// minDistances[node.get(w).getId()]);
+						minDistances[node.get(w).getId()] = minDistances[node
+								.get(minV).getId()]
+								+ distance[node.get(minV).getId()][node.get(w)
+										.getId()];
 						workpath[node.get(w).getId()] = node.get(minV).getId();
-						//System.out.println(w + " - " + minDistances[w]);
-						//System.out.println("set parent: " + node.get(minV).getId() + " - " + node.get(w).getId());
+						// System.out.println(w + " - " + minDistances[w]);
+						// System.out.println("set parent: " +
+						// node.get(minV).getId() + " - " +
+						// node.get(w).getId());
 					}
 				}
 			}
 		}
-//		 System.out.println("sadkjshdkjashsakjdhsajdhajka");
-		 while (dest != src) {
-		 workpath1.add(dest);
-//		 System.out.print(dest + "-");
-		 dest = node.get(workpath[dest.getId()]);
-		 }
-//		for (int i = 0; i < node.size(); i++) {
-//			System.out.println("Parent of " + node.get(i).getId() + ": " + workpath[node.get(i).getId()]);
-//		}
+		// System.out.println("sadkjshdkjashsakjdhsajdhajka");
+		while (dest != src) {
+			workpath1.add(dest);
+			// System.out.print(dest + "-");
+			dest = node.get(workpath[dest.getId()]);
+		}
+		// for (int i = 0; i < node.size(); i++) {
+		// System.out.println("Parent of " + node.get(i).getId() + ": " +
+		// workpath[node.get(i).getId()]);
+		// }
 		workpath1.add(src);
-//		 System.out.print(src);
+
+		for (int i = 0; i < node.size(); i++) {
+			for (int j = 0; j < node.size(); j++) {
+				weight[i][j] = distance[i][j];
+			}
+		}
+
+		for (int i = 0; i <= workpath1.size() - 2; i++) {
+			// System.out.println(i);
+			weight[workpath1.get(i).getId()][workpath1.get(i + 1).getId()] = Float.MAX_VALUE;
+			weight[workpath1.get(i + 1).getId()][workpath1.get(i).getId()] = Float.MAX_VALUE;
+			// System.out.println(workpath1.get(i).getId() + " " +
+			// workpath1.get(i+1).getId() + " "+
+			// weight[workpath1.get(i).getId()][workpath1.get(i+1).getId()]);
+		}
+		for (int i = 0; i < node.size(); i++) {
+			for (int j = 0; j < node.size(); j++) {
+				if (weight[i][j] != Float.MAX_VALUE && connected[i][j]) {
+					weight[i][j] = 0;
+					weight[j][i] = 0;
+				}
+			}
+		}
+		// for (int i = 0; i < node.size(); i++) {
+		// for (int j = 0; j < node.size(); j++) {
+		// System.out.format("%.2f \t", weight[i][j]);
+		// }
+		// System.out.println();
+		// }
+		// System.out.println("mang that");
+		// for (int i = 0; i < node.size(); i++) {
+		// for (int j = 0; j < node.size(); j++) {
+		// System.out.format("%.2f \t", distance[i][j]);
+		// }
+		// System.out.println();
+		// }
+		// System.out.print(src);
 		return workpath1;
 	}
 
@@ -176,8 +225,8 @@ public class Graph {
 	private boolean isValidNode(Node u) {
 		return (u.getId() >= 0) && (u.getId() <= this.v - 1);
 	}
-	
-	public float Cost(){
+
+	public float Cost() {
 		float s = 0;
 		for (int i = 0; i < edge.size(); i++) {
 			s += distance[edge.get(i).getSource()][edge.get(i).getDestination()];
--- src/Node.java
+++ src/Node.java
@@ -1,40 +1,48 @@
-
 public class Node {
 	private int x;
 	private int y;
-//	private String name;
+	// private String name;
 	private int id;
+
 	public Node(int x, int y) {
 		super();
 		this.x = x;
 		this.y = y;
 	}
-	public Node(int x, int y, int id){
+
+	public Node(int x, int y, int id) {
 		super();
 		this.x = x;
 		this.y = y;
 		this.id = id;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getX() {
 		return x;
 	}
+
 	public void setX(int x) {
 		this.x = x;
 	}
+
 	public int getY() {
 		return y;
 	}
+
 	public void setY(int y) {
 		this.y = y;
 	}
+
 	@Override
 	public String toString() {
-		return  "Node"+id+" :[x=" + x + ", y=" + y + "]";
+		return "Node" + id + " :[x=" + x + ", y=" + y + "]";
 	}
 }
